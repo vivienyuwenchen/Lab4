22cb1cbb6ba46da836698ec246d2a0c567c6d4b4
vivienyuwenchen <vivienyuwenchen@gmail.com>
2018-11-12 21:25:06 -0500

    Surgically insert forwarding lut

--- verilog/forwardingLUT.v
+++ verilog/forwardingLUT.v
@@ -1,25 +1,24 @@
 //------------------------------------------------------------------------
 // Fowarding Look Up Table
 // Mux Control   | Source | Explanation
-// ForwardA = 00 | EX     | The first ALU operand comes from the register fi le.
+// ForwardA = 00 | EX     | The first ALU operand comes from the register file.
 // ForwardA = 10 | MEM    | The first ALU operand is forwarded from the prior ALU result.
 // ForwardA = 01 | WB     | The first ALU operand is forwarded from data memory or an earlier ALU result.
-// ForwardB = 00 | EX     | The second ALU operand comes from the register fi le.
+// ForwardB = 00 | EX     | The second ALU operand comes from the register file.
 // ForwardB = 10 | MEM    | The second ALU operand is forwarded from the prior ALU result.
 // ForwardB = 01 | WB     | The second ALU operand is forwarded from data memory or an earlier ALU result.
 
 //Companion reading chapter 4.7 pg 370
 //------------------------------------------------------------------------
 
-module fowardingLUT
+module forwardingLUT
 (
-    input [31:0] ex_rs,
-    input [31:0] ex_rt,
-    input [31:0] mem_regRd,
-    input [31:0] wb_regRd,
-    input [31:0] wb_rd,
-    input [31:0] rs,
-    input [31:0] rt,
+    input [4:0] ex_rs,
+    input [4:0] ex_rt,
+    input [4:0] mem_regRd,
+    input [4:0] wb_regRd,
+    input [4:0] rs,
+    input [4:0] rt,
     input       mem_regWrite,
     input       wb_regWrite,
     output reg [1:0] forwardA,
