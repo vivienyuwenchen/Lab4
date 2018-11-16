3f5210f8ea0eaf80177f32a1c72e1bb6f8ab9de2
Samantha Young <samantha.young@students.olin.edu>
2018-11-15 19:54:57 -0500

    adding branch

--- asm/beqbne.asm
+++ asm/beqbne.asm
@@ -1,10 +1,11 @@
 addi $a0, $zero, 1 #n
 addi $t0, $zero, 1 #num
 addi $t1, $zero, 0 #sum
+addi $t2, $zero, 0 #sum
 
 beq $t0, $a0, ENDIF
 add $t1, $t1, 1 
 
 ENDIF:
-add $t1, $t1, 2
+add $t2, $t2, 2
 
